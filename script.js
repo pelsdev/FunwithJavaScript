a783e160c1febcd2fc4cc26d950e9fbba1e53d73
pelsdev <pelumiajiteru@gmail.com>
2024-02-05 16:43:21 +0100

    Coding challenge 1

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 let js = "amazing";
 if (js === "amazing") alert("JavaScript is FUN!");
+
+// coding challenge 1
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log (BMIMark, BMIJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log (markHigherBMI);
