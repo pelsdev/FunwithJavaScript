eee39b2a468365550a1e9660fbde47a5264955fe
pelsdev <pelumiajiteru@gmail.com>
2024-02-12 13:36:57 +0100

    Use of Ternary Operator

--- script.js
+++ script.js
@@ -34,3 +34,9 @@ if (dolphineScore > koalasScore && dolphineScore >= 100){
 } else {
     console.log('There was no winner')
 }
+
+// coding challenge 4
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
